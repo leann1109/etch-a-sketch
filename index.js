33794edc94263444bef9139abfbe0239eb72c203
Lean Obregon <lean.obregon1@gmail.com>
2023-07-01 12:53:05 -0400

    re-did addGrid function

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const container = document.querySelector(".container");
 const divGrid = document.createElement("div");
-divGrid.classList.add("divGrid");
 
-
-let i = 0;
 function addGrid(numTimes) {
+    let i = 0;
+    let gridNum = 0;
+    let divGrid = "divGrid";
+    let currentGrid = divGrid + gridNum;
     do {
-        container.appendChild(divGrid.cloneNode(true));
+        currentGrid = document.createElement("div");
+        currentGrid.classList.add(divGrid + gridNum);
+        currentGrid.setAttribute("id",divGrid + gridNum);
+        container.appendChild(currentGrid);
         i++
+        gridNum ++;
     }
     while (i < numTimes);
 }
 
-console.log(addGrid(16*16));
+function colorGrid() {
+    document.getElementById("divGrid").style.backgroundColor = "red";
+}
+
+addGrid(16*16);
+// const divs = Array.from(document.querySelectorAll(".divGrid"));
+// window.addEventListener("mouseover", colorGrid);
